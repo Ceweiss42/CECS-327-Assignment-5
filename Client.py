da49e8a24969143c3e64ea9bf322d01b1d54c1db
connorkoch0511 <connor.koch@student.csulb.edu>
2023-04-05 13:30:06 -0700

    Add error handler to check for valid ip address

--- Client.py
+++ Client.py
@@ -1,28 +1,48 @@
 import socket
-from time import sleep
+import ipaddress
 
 class Client:
 
-    def __init__(self, ip: str, port: int, message: str):                     # constructor for Client class
+    # constructor for Client class
+    def __init__(self, ip: str, port: int, message: str):                
         self.ip = ip
         self.port = port
         self.message = message
     
-    def clientToServer(self):                                                 # function to send message from client to server
+    # function to send message from client to server
+    def clientToServer(self):                                                 
         while True:
-            s = socket.socket()                                               # create a socket
-            s.connect((self.ip, self.port))                                   # connect socket using ip address and port number
-            s.sendall(bytes(self.message, 'utf-8'))                           # send message to the Server
+            # create a socket
+            s = socket.socket() 
+            # connect socket using ip address and port number                                              
+            s.connect((self.ip, self.port))     
+            # send message to the Server                              
+            s.sendall(bytes(self.message, 'utf-8'))                           
 
-            m = s.recv(64).decode('utf-8')                                    # recieve a message from the Server
+            # recieve a message from the Server
+            m = s.recv(64).decode('utf-8')                                    
             print('Message received from Server:' + m)
 
             s.close()
-            if input("Would you like to close the program?(Y/N): ") == "Y":   # break the infinite loop if the user does not want to send anymore messages
+            # break the infinite loop if the user does not want to send anymore messages
+            if input("Would you like to close the program?(Y/N): ") == "Y":   
                 break
-            self.message = input("please enter a message: ")                  # enter a new message to send to the Server
+            # enter a new message to send to the Server
+            self.message = input("please enter a message: ")                  
 
 
 if __name__ == "__main__":
-    client = Client(input("please enter an ip: "), int(input("please enter a port: ")), input("please enter a message: "))
+    # infinite loop, until a valid IP address is entered
+    while True:                                                               
+        try:
+            ip = input("please enter an ip: ")
+            # checks if IP address is valid
+            checkIP = ipaddress.ip_address(ip)                                
+            break
+        except ValueError:
+            print("Error: Not an IP address, please enter a valid IP address")
+    port = int(input("please enter a port: "))
+    message = input("please enter a message: ")
+    client = Client(ip, port, message)
+    #client = Client(input("please enter an ip: "), int(input("please enter a port: ")), input("please enter a message: "))
     client.clientToServer()
